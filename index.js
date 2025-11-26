3ffe05de3a4fa7afe6a6f4bdd3e5d92d2e7d4f1a
Zelenov Konstantin <KonstantinZelenovViga@yandex.com>
2025-11-26 16:03:40 +0400

    more fixes

--- index.js
+++ index.js
@@ -4,19 +4,13 @@ import { initPopups } from './scripts/popupManager.js';
 import { initAnimations } from './scripts/animatIcons.js';
 import { ThemeSwitcher } from './scripts/theme-switcher.js';
 
-function setViewportHeight() {
-  document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   initPopups();
   initContactForm();
-  setViewportHeight();
   initAnimations();
   new ThemeSwitcher();
   new SmoothMenuWheel('.popup__wheel-container');
-  window.addEventListener('resize', setViewportHeight);
 });
 
-window.addEventListener('load', setViewportHeight);
+
 
